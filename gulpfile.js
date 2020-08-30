8c657f4bf1513ed379c0376fa398d0d3884b34e7
Nikolay <noda.nikolay@gmail.com>
2020-08-30 18:46:01 +0300

    111

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
-// !Gulp
-const { src, dest, series, parallel, watch } = require("gulp");
 
-const browsersync = require("browser-sync").create();
+const { src, dest, series, parallel, watch } = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const notify = require('gulp-notify');
+const include = require('gulp-include');
+const autoprefixer = require('gulp-autoprefixer');
+const browsersync = require('browser-sync').create();
+
 const rename = require("gulp-rename");
 const fileinclude = require("gulp-file-include");
 const del = require("del");
 
-// !	Styles
 const scss = require("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
+// const autoprefixer = require("gulp-autoprefixer");
 const groupcssmediaqueries = require("gulp-group-css-media-queries");
 const cleancss = require("gulp-clean-css");
 
-// !	JS
+
 const uglify = require("gulp-uglify-es").default;
 
-// !Images
+
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const webphtml = require("gulp-webp-html");
@@ -32,7 +36,8 @@ const FONTS_SUBDIR = 'fonts';
 
 const path = {
 	source: {
-		html: [`${SOURCE_DIR}**/*.html`, `!**${INCLUDE_SUBDIR}**`],
+		dir: `./${SOURCE_DIR}/`,
+		html: [`./${SOURCE_DIR}/**/*.html`, `!**/${INCLUDE_SUBDIR}/**`],
 		styles: [`${SOURCE_DIR}/${STYLES_SUBDIR}/*.scss`, `!**${INCLUDE_SUBDIR}**`],
 		scripts: [`${SOURCE_DIR}/${SCRIPTS_SUBDIR}/*.js`, `!**${INCLUDE_SUBDIR}**`],
 		images: `${SOURCE_DIR}/${IMAGES_SUBDIR}/**/*.{jpg,png,gif,webp}`,
@@ -48,14 +53,14 @@ const path = {
 	},
 	build: {
 		dir: `./${BUILD_DIR}/`,
-		html: `${BUILD_DIR}/`,
+		html: `./${BUILD_DIR}/`,
 		styles: `${BUILD_DIR}/${STYLES_SUBDIR}/`,
 		scripts: `${BUILD_DIR}/${SCRIPTS_SUBDIR}/`,
 		images: `${BUILD_DIR}/${IMAGES_SUBDIR}/`,
 		fonts: `${BUILD_DIR}/${FONTS_SUBDIR}/`
 	},
 	watch: {
-		html: SOURCE_DIR + "/**/*.html",
+		html: `./${SOURCE_DIR}/**/*.html`,
 		styles: `${SOURCE_DIR}/${STYLES_SUBDIR}/*.scss`,
 		scripts: `${SOURCE_DIR}/${SCRIPTS_SUBDIR}/*.js`,
 		images: `${SOURCE_DIR}/${IMAGES_SUBDIR}/**/*.{jpg,png,gif,webp}`,
@@ -63,28 +68,65 @@ const path = {
 	}
 };
 
-// 
+const htmlBuild = () => {
+	return src(path.source.html)
+		.pipe(include())
+		.pipe(dest(`./${BUILD_DIR}/`))
+}
 
-const browserSync = () => {
+const htmlRelease = () => {
+	return src(path.build.html)
+		.pipe(dest(path.release.html))
+}
+
+const watchHtml = () => {
+	return watch(path.watch.html)
+		.on('change', series(htmlBuild, parallel(htmlRelease, browserSyncReload)))
+}
+
+const styles = () => {
+	return src([`./${SOURCE_DIR}/${STYLES_DIR}/**/*.scss`, `!**/${INCLUDE_DIR}/**/*.scss`])
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}).on('error', sass.logError))
+		.pipe(include())
+		.on('error', sass.logError)
+		.pipe(autoprefixer())
+		.pipe(dest(`./${BUILD_DIR}/${STYLES_DIR}`))
+	// .pipe(notify('Styles processed'))
+}
+
+const stylesRelease = () => (
+	src(`./${BUILD_DIR}/${STYLES_DIR}/**/*.css`)
+		.pipe(dest(`./${RELEASE_DIR}/${STYLES_DIR}`))
+)
+
+const watchCSS = () => {
+	watch(`./${SOURCE_DIR}/${STYLES_DIR}/**/*.scss`)
+		.on('change', series(styles, parallel(stylesRelease, () => (browsersync.stream({ match: '**/*.css' })))));
+}
+
+const browserSyncInit = (cb) => {
 	browsersync.init({
 		server: {
-			baseDir: path.build.dir,
-			https: false,
-			notify: false
+			baseDir: `./${BUILD_DIR}/`
 		},
 		port: 5000,
 		notify: false
-	})
+	});
+	cb();
 }
 
-function html() {
-	return src(path.source.html)
-		.pipe(fileinclude())
-		.pipe(webphtml())
-		.pipe(dest(path.pub.html))
-		.pipe(browsersync.stream());
-	// .pipe(browsersync.reload());
+const browserSyncReload = (cb) => {
+	browsersync.reload();
+	cb();
 }
+
+exports.html = htmlBuild;
+exports.default = series(htmlBuild, parallel(htmlRelease, browserSyncInit), watchHtml);
+// exports.default = series(htmlBuild);
+
+// 
 
 const stylesTask = () => {
 	return src(path.src.styles)
@@ -171,14 +213,5 @@ function clean() {
 	return del(path.clean_pub);
 }
 
-let build = series(clean, parallel(img, js, stylesTask, html));
-let watch1 = parallel(build, watchFiles, browserSync);
-
-exports.html = html;
-exports.styles = stylesTask;
-exports.js = js;
-exports.img = img;
-exports.build = build;
-exports.watch = watch1;
-// exports.svgSprite = svgSprite;
-exports.default = watch1;
+// let build = series(clean, parallel(img, js, stylesTask, html));
+// let watch1 = parallel(build, watchFiles, browserSync);
